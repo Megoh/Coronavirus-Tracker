b70402aa824661c4b0f51cb6865308929d491e31
Dominik <dominikwolowicz@gmail.com>
2022-07-19 10:25:43 +0200

    fix gitignore and print data on app start

--- src/main/java/pl/dominik/coronavirustracker/services/CoronavirusDataService.java
+++ src/main/java/pl/dominik/coronavirustracker/services/CoronavirusDataService.java
@@ -1,11 +1,23 @@
 package pl.dominik.coronavirustracker.services;
 
-import java.net.http.HttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
 public class CoronavirusDataService {
     private static final String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-    public void fetchVirusData() {
+    @Bean
+    public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(VIRUS_DATA_URL)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
